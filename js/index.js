15bae9cef070a6bf28b702051f04601a33af92cb
Camila Arena <arenabelu@gmail.com>
2022-07-13 22:38:53 -0300

    comentario 1

---
+++ js/index.js
@@ -0,0 +1,5 @@
+
+ let numeroParaRepeticion=parseInt(prompt("Ingrese las veces que quiera repetir el saludo"))
+ for( i= 0 ; i< numeroParaRepeticion ; i++){
+    alert(`Hola bienvenido querido usuario,este es el saludo numero ${i}`)
+ }
